f890ca6225ddad5f08022368908cedfbe5578e2c
bhan <bhan@idevel.kr>
2025-03-15 14:40:14 -0700

    1679.Max-Number-of-K-Sum-Pairs

--- LeetCode75/11.Container-With-Most-Water.js
+++ LeetCode75/11.Container-With-Most-Water.js
@@ -1,36 +1,48 @@
 /*
-Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
-A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
 
  
 
 Example 1:
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
-Input: s = "abc", t = "ahbgdc"
-Output: true
 Example 2:
-
-Input: s = "axc", t = "ahbgdc"
-Output: false
+Input: height = [1,1]
+Output: 1
 */
 
 /**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * @param {number[]} height
+ * @return {number}
  */
-var isSubsequence = function (s, t) {
-    let idx = 0;
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
 
-    for (let i = 0; i < t.length; i++) {
-        if (s[idx] === t[i]) {
-            idx++
+    while (left < right) {
+        let width = right - left;
+        let minHeight = Math.min(height[left], height[right]);
+        let area = width * minHeight;
+        
+        // Update max water if the current area is larger
+        maxWater = Math.max(maxWater, area);
+        
+        // Move the pointer of the smaller height
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
         }
-        if (idx === s.length) return true;
-
     }
 
-    return idx === s.length;
-
+    return maxWater;
 };
